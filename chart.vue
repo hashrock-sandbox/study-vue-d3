1c3e44eac6477b3c1df65660012a4b8c2d34bebd
hashrock <hashedrock@gmail.com>
2015-12-04 20:22:50 +0900

    change to image

--- chart.vue
+++ chart.vue
@@ -1,9 +1,18 @@
 <style>
 
+  .node {
+    stroke-width: 1.5px;
+  }
+  svg{
+    border: solid 1px black;
+    background: #99FF5F;
+  }
+
 </style>
 
 <template>
   <p></p>
+  <svg width="600px" height="600px"></svg>
 </template>
 
 <script>
@@ -18,6 +27,10 @@
     this.$el.text(data);
   }
   var chart;
+  
+  var node;
+  var width = 600;
+  var height = 600;
 
   module.exports = {
     props: ["input"],
@@ -32,6 +45,38 @@
     ready: function(){
       chart = new Chart("p");
       chart.update(this.input);
+      function tick() {
+        node.attr("x", function(d) { return d.x; })
+            .attr("y", function(d) { return d.y; });
+      }
+      
+      var nodes = d3.range(30).map(function(i) {
+        return {index: i};
+      });
+      var force = d3.layout.force()
+          .nodes(nodes)
+          .size([width, height])
+          .charge(-1000)
+          .on("tick", tick)
+          .start();
+          
+      var svg = d3.select("svg");
+      
+      node = svg.selectAll(".node")
+        .data(nodes)
+        .enter().append("image")
+        .attr("class", "node")
+        .attr("xlink:href", "cloud-hi.png")
+        .attr("x", function(d){return d.x})
+        .attr("y", function(d){return d.y})
+        .attr("height", "200px")
+        .attr("width", "200px")
+        //.attr("cx", function(d){return d.x})
+        //.attr("cy", function(d){return d.y})
+        //.attr("r", 16)
+        .call(force.drag);
+        
+        
     }
   }
 </script>
