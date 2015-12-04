7693e7b82fb247ef805bf6f4ad015b8362774b5a
hashrock <hashedrock@gmail.com>
2015-12-04 21:56:22 +0900

    add Images

--- chart.vue
+++ chart.vue
@@ -21,11 +21,39 @@
   var Chart = function(el){
     this.el = el;
     this.$el = d3.select(el);
+    this.nodes = [];
+    this.nodes = d3.range(30).map(function(i) {
+      return {index: i};
+    });
   }
   
   Chart.prototype.update = function(data){
-    this.$el.text(data);
+    function tick() {
+      node.attr("x", function(d) { return d.x; })
+          .attr("y", function(d) { return d.y; });
+    }
+    
+    var force = d3.layout.force()
+        .nodes(this.nodes)
+        .size([width, height])
+        .charge(-1000)
+        .on("tick", tick)
+        .start();
+        
+    var svg = d3.select("svg");
+    
+    node = svg.selectAll(".node")
+      .data(this.nodes)
+      .enter().append("image")
+      .attr("class", "node")
+      .attr("xlink:href", "cloud-hi.png")
+      .attr("x", function(d){return d.x})
+      .attr("y", function(d){return d.y})
+      .attr("height", "200px")
+      .attr("width", "200px")
+      .call(force.drag);
   }
+  
   var chart;
   
   var node;
@@ -43,40 +71,8 @@
       }
     },
     ready: function(){
-      chart = new Chart("p");
+      chart = new Chart("svg");
       chart.update(this.input);
-      function tick() {
-        node.attr("x", function(d) { return d.x; })
-            .attr("y", function(d) { return d.y; });
-      }
-      
-      var nodes = d3.range(30).map(function(i) {
-        return {index: i};
-      });
-      var force = d3.layout.force()
-          .nodes(nodes)
-          .size([width, height])
-          .charge(-1000)
-          .on("tick", tick)
-          .start();
-          
-      var svg = d3.select("svg");
-      
-      node = svg.selectAll(".node")
-        .data(nodes)
-        .enter().append("image")
-        .attr("class", "node")
-        .attr("xlink:href", "cloud-hi.png")
-        .attr("x", function(d){return d.x})
-        .attr("y", function(d){return d.y})
-        .attr("height", "200px")
-        .attr("width", "200px")
-        //.attr("cx", function(d){return d.x})
-        //.attr("cy", function(d){return d.y})
-        //.attr("r", 16)
-        .call(force.drag);
-        
-        
     }
   }
 </script>
