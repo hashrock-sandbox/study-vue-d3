841b7e57208a7e12a20babcddfabfac05c174be6
hashrock <hashedrock@gmail.com>
2015-12-04 19:54:17 +0900

    initial commit

---
+++ app.vue
@@ -0,0 +1,22 @@
+<style>
+</style>
+
+<template>
+  <input type="text" v-model="text">
+  <chart :input="text"></chart>
+</template>
+
+<script>
+  var chart;
+  module.exports = {
+    data: function () {
+      return {
+        msg: 'Hello world!',
+        text: "hello"
+      }
+    },
+    components: {
+      chart : require("./chart.vue")
+    }
+  }
+</script>
---
+++ chart.vue
@@ -0,0 +1,37 @@
+<style>
+
+</style>
+
+<template>
+  <p></p>
+</template>
+
+<script>
+  var d3 = require("d3");
+  
+  var Chart = function(el){
+    this.el = el;
+    this.$el = d3.select(el);
+  }
+  
+  Chart.prototype.update = function(data){
+    this.$el.text(data);
+  }
+  var chart;
+
+  module.exports = {
+    props: ["input"],
+    data: function () {
+      return {}
+    },
+    watch: {
+      "input" : function(val){
+        chart.update(val);
+      }
+    },
+    ready: function(){
+      chart = new Chart("p");
+      chart.update(this.input);
+    }
+  }
+</script>
